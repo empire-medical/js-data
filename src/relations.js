749dab6a23ba26b1a5b0ed1f896935e6aebf8236
Tobias Lounsbury <TobiasLounsbury@gmail.com>
2022-09-23 13:31:16 -0700

    Refactored the imports and exports to reflect changes made in the Relations

--- src/relations.js
+++ src/relations.js
@@ -1,10 +1,4 @@
-import { Relation } from './Relation'
-import { BelongsToRelation, belongsToType } from './Relation/BelongsTo'
-import { HasManyRelation, hasManyType } from './Relation/HasMany'
-import { HasOneRelation, hasOneType } from './Relation/HasOne'
-
-[BelongsToRelation, HasManyRelation, HasOneRelation].forEach(RelationType => {
-  Relation[RelationType.TYPE_NAME] = (related, options) => new RelationType(related, options)
-})
-
-export { belongsToType, hasManyType, hasOneType, Relation }
+export { Relation } from './Relation'
+export { belongsToType } from './Relation/BelongsTo'
+export { hasManyType } from './Relation/HasMany'
+export { hasOneType } from './Relation/HasOne'
